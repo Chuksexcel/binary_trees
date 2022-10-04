1ae1243702c284b5072d1da7959873b4aa88a23a
Chukwuemeka <mekabanz@gmail.com>
2022-10-04 02:39:26 -0700

    Add task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,17 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree to count the leaves from
  *
- * Return: size of the tree
+ * Return: number of leaves
  *         0 if tree is NULL
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}	
+	if (!tree->left && !tree->right)
+		return (1);
 
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
